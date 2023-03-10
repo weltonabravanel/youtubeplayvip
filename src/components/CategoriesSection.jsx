6e1d3cffae85ccaf5a5703e17785e7ce70f3e6b1
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-10 11:26:28 -0300

    Update CategoriesSection.jsx

--- src/components/CategoriesSection.jsx
+++ src/components/CategoriesSection.jsx
@@ -6,13 +6,13 @@ const CategoriesSection = () => {
   return (
     <div className="xl:flex items-center xl:max-w-[60%] shrink max-w-[40%] hidden  overflow-x-scrol space-x-14 scrollbar-hide md:pl-0 pl-5 md:pr-5">
     <Link to="/"><Categories title="All" id='' handleClick="" /></Link>
-    <Categories title="Trending" id="Trending" handleClick="" />
-    <Categories title="Music" id="music"  handleClick="" />
-    <Categories title="News" id="news" handleClick="" />
-     <Categories title="Gaming" id="gaming" handleClick="" />
-    <Categories title="Sports" id="sports" handleClick="" />
-    <Categories title="Films" id="movies" handleClick="" />
-    <Categories title="Fashion" id="26" handleClick="" />
+    <Categories title="Tendências" id="Trending" handleClick="" />
+    <Categories title="Musicas" id="music"  handleClick="" />
+    <Categories title="Notícias" id="news" handleClick="" />
+     <Categories title="Jogos" id="gaming" handleClick="" />
+    <Categories title="Esportes" id="sports" handleClick="" />
+    <Categories title="Filmes" id="movies" handleClick="" />
+    <Categories title="Estilo" id="26" handleClick="" />
     {/* <Categories title="Comedy" id="23" handleClick="" /> */}
     {/* <Categories title="DOTA" id="20" handleClick="" /> */}
   </div>
