bafb9335860eddc5b9da00fbfcdf0c9a77134700
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-10 15:36:01 -0300

    Update SidebarExploreSection.jsx

--- src/components/SidebarExploreSection.jsx
+++ src/components/SidebarExploreSection.jsx
@@ -112,7 +112,7 @@ const SidebarExploreSection = () => {
           icon={
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-brand-tidal"
+              class="icon icon-tabler icon-tabler-brand-tv"
               width="20"
               height="20"
               viewBox="0 0 24 24"
