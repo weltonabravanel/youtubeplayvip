f70d359ad4beb61d48c0fe23f3b5224a47e9dbc8
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-10 15:26:55 -0300

    Update SidebarExploreSection.jsx

--- src/components/SidebarExploreSection.jsx
+++ src/components/SidebarExploreSection.jsx
@@ -86,7 +86,7 @@ const SidebarExploreSection = () => {
               <path d="M5.333 6l3.334 3.25l3.333 -3.25l3.333 3.25l3.334 -3.25l3.333 3.25l-3.333 3.25l-3.334 -3.25l-3.333 3.25l3.333 3.25l-3.333 3.25l-3.333 -3.25l3.333 -3.25l-3.333 -3.25l-3.334 3.25l-3.333 -3.25z"></path>
             </svg>
           }
-          title="Shows"
+          title="Televisão"
         />
         <SideBarExploreItem
           icon={
