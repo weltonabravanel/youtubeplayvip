4067a6e63e5b2d45a689671c409219f29e65fbdf
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-11 17:30:46 -0300

    Update Feed.jsx

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -12,7 +12,7 @@ const Feed = () => {
 
 
 useEffect(()=>{
- api(`trending?maxResults=10`).then((response)=>setData(response))
+ api(`sports?maxResults=10`).then((response)=>setData(response))
 },[])
 console.log(data)
    
